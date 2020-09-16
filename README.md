eba02bb52e07c410a865da15aa3fd1b3d27ffa07
Tristan <itstristantaylor@gmail.com>
2020-09-16 14:35:10 -0400

    Initial Load
    Created README and gitignore, as well as the first .py file of functions for use in the main script. These are all documented both in the module and the README.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # FBProgrammingPuzzle
  Repo for GIS Analyst Programming Puzzle
+
+## Scripts:
+### 1) ExtractAndZipKMZFiles.py
+This module contains functions for extracting and zipping KMZ files to KML and vice versa.
+- #### GetUserInputKMZFile: 
+    This function is used twice within ExtractKMZToKML. It uses basic text input and a while loop to validate the input, copying the user-input file from wherever it resides into the KMZ_Sourcefile folder of the repo structure.
+- #### ExtractKMZToKML: 
+    This function assumes the repo folder structure is in use. It sets the OS directory 2 levels up from itself and proceeds to identify the KMZ file to be extracted. If one or more is found in the KMZ_Sourcefile folder, it is assumed the user intends to use one of them since the repo clone has already been set up on their machine. If the KMZ_Sourcefile folder does not exist or is empty, the script will request that the user provides the path to the KMZ file they would like copied in and extracted via simple text input. If more than one KMZ file is present in the KMZ_Sourcefile folder, the script will ask the user to specify which one to use by inputting an index to match the enumerated list. 
+- #### Compre3ssKMLFileToKMZ:
+    This function takes in the desired KMZ file name (i.e. Output.kmz) and, optionally, a specified KML file name (i.e. output.kml) and writes the KML file to the _Output subfolder of the repo folder structure, creating the subfolder if necessary. If no KML file is specified, it assumes the user intends to pack up doc.kml. The KML file **must** be in the KML_Sourcefile folder.
+
+### [TODO] ParseAndModifyKML.py
+
+### [TODO] RunProgrammingPuzzle.bat
---
+++ src/ExtractAndZipKMZFiles.py
@@ -0,0 +1,72 @@
+import zipfile
+import os
+
+def GetUserInputKMZFile():
+    while True:
+        infile = rf"{input('Please enter the path to your source KMZ file here: ')}"
+        if os.path.exists(infile) and infile[-4:] == ".kmz":
+            baseName = os.path.basename(infile)
+            newPath = os.path.join("KMZ_Sourcefile", baseName)
+            os.rename(infile, newPath)
+            break
+        else:
+            print("Either the file you entered does not exist, or it's not a KMZ file. Please try again!")
+    return baseName
+
+
+def ExtractKMZToKML():
+    ## Get the filepath to the base repo folder and set it as the OS directory
+    basefolder = os.path.dirname(os.path.dirname(__file__))
+    os.chdir(basefolder)
+
+    ## Verify that the folder for source data exists
+    if os.path.exists("KMZ_Sourcefile"):
+        ## Pull existing files into a list
+        existingFiles = os.listdir("KMZ_Sourcefile")
+        ## Narrow down to kmzFiles
+        kmzFiles = [f for f in existingFiles if ".kmz" in f]
+        ## Get specified file, either the only one if there IS only one, or by user input
+        if len(kmzFiles) > 1:
+            ## Get the kmz file the user wants. Must ensure input is castable to int for indexing
+            while True:
+                fileIndex = rf"{input(f'Please enter the index of the KMZ you wish to unzip from the following list: {list(enumerate(kmzFiles))}  ')}"
+                try:
+                    fileIndex = int(fileIndex)
+                    if fileIndex in range(len(kmzFiles)):
+                        break
+                    else:
+                        print("Value entered fell outside the available indices. Please try again.")
+                except:
+                    print("Value entered could not be cast to Int! Please try again.")
+            finalKMZ = kmzFiles[fileIndex]
+        elif len(kmzFiles) == 1:
+            finalKMZ = kmzFiles[0]
+        ## If no KMZ files are found, go the user input route like if the folder doesn't exist (below)
+        else:
+            finalKMZ = GetUserInputKMZFile()
+    ## If the KMZ_Sourcefile folder doesn't exist, create it and have the user 
+    ## specify the source file to ccut/paste into this folder
+    else:
+        os.mkdir("KMZ_Sourcefile")
+        finalKMZ = GetUserInputKMZFile()
+
+    ## If a doc.kml file was previously extracted, delete it now (it's source KMZ will be untouched)
+    if os.path.exists("KMZ_Sourcefile/doc.kml"):
+        os.remove("KMZ_Sourcefile/doc.kml")
+
+    ## Unzip KMZ file in the KMZ_Sourcefile folder to expose doc.kml
+    with zipfile.ZipFile(os.path.join("KMZ_Sourcefile", finalKMZ),'r') as zip_ref:
+        zip_ref.extractall("KMZ_Sourcefile")
+
+
+def CompressKMLToKMZ(KMZName, KMLName="doc.kml"):
+    """
+    Keyword Args:
+    - KMZName: Any string name to be applied to the KMZ file. The KML file within will retain its name
+    - KMLName: The string name of the KML file to be written. By default, it will be named doc.kml
+    """
+    ## Create ZipFile object in with structure and add KMLName to the zip repository
+    if not os.path.exists("_Output"):
+        os.mkdir("_Output")
+    with zipfile.ZipFile(rf"_Output\{KMZName}", 'w') as zip_ref:
+        zip_ref.write(rf"KMZ_Sourcefile\{KMLName}")
